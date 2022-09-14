aa167ca366d06df317263666a7944cb6771cf04e
Palura Andrei <paluraa@yahoo.com>
2022-09-14 19:40:38 +0300

    Changed the logic of the game

--- rps.js
+++ rps.js
@@ -1,31 +1,51 @@
-const rpsArray = ["rock", "paper", "scissors"];
+const playerText = document.querySelector("#playerText");
+const computerText = document.querySelector('#computerText');
+const resultText = document.querySelector("#resultText");
+const choiceBtn = document.querySelectorAll('.choiceBtn')
 
-var tie = 0;
-var player = 0;
-var computer = 0;
+let playerVar;
+let computerVar;
+let resultVar;
 
-let playerSelection = playerPlay();
-let computerSelection = computerPlay()
-console.log(tie)
+choiceBtn.forEach(button => button.addEventListener("click", ()=>
+{
+    playerVar=button.textContent;
+    computerTurn();
+    playerText.textContent= `Player :${playerVar}`;
+    computerText.textContent= `Computer :${computerVar}`;
+    resultText.textContent= winners()
+}))
 
-// Computer random pick
-function computerPlay() {
-   return rpsArray[Math.floor(Math.random() * rpsArray.length)];
-} 
-const
-
-function playerPlay(){
-    return "rock"
-}
-
-
-//Round rules
-function playRound(playerSelection,computerSelection){
-
-    if (playerSelection == computerSelection){
-        return tie++
+function computerTurn(){
+    const choice = (Math.floor(Math.random() * 3)+ 1) ;
+    switch(choice){
+        case 1:
+            computerVar= "Rock";
+            break;
+        case 2:
+            computerVar= "Paper";
+            break;  
+        case 3:
+            computerVar= "Scissor";      
     }
 
 
 }
-console.log(playRound("rock","rock"))
+
+function winners(){
+    if (playerVar === computerVar){
+        return "Tie";
+    }else if (playerVar == "Rock" && computerVar == "Paper"){
+        return "Lost";
+    }else if (playerVar == "Scissor" && computerVar == "Rock"){
+            return "Lost";
+    }else if (playerVar == "Paper" && computerVar == "Scissor"){
+            return "Lost";
+    }else if (playerVar == "Scissor" && computerVar == "Paper"){
+            return "Win";
+    }else if (playerVar == "Rock" && computerVar == "Scissor"){
+            return "Win";
+    }else if (playerVar == "Paper" && computerVar == "Rock"){
+            return "Win";
+}
+}
